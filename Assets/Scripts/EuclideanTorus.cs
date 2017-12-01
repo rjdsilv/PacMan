96fede9acff4254d0fefd9fd708873c193873eb5
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-11-30 22:31:11 -0500

    Finished the Animations.

---
+++ Assets/Scripts/EuclideanTorus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EuclideanTorus : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+		if (transform.position.x > 5.3f)
+        {
+            transform.position = new Vector3(-5.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -5.3f)
+        {
+            transform.position = new Vector3(5.3f, transform.position.y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PacManController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody2D pacManRigidBody;
+    private Animator pacManAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pacManRigidBody = GetComponent<Rigidbody2D>();
+        pacManAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayProperAnimation();
+	}
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        pacManRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+    }
+
+    private void PlayProperAnimation()
+    {
+        if (IsGoingRight())
+        {
+            pacManAnimator.Play("PacManRight");
+        }
+        else if (IsGoingLeft())
+        {
+            pacManAnimator.Play("PacManLeft");
+        }
+        else if (IsGoingUp())
+        {
+            pacManAnimator.Play("PacManUp");
+        }
+        else if (IsGoingDown())
+        {
+            pacManAnimator.Play("PacManDown");
+        }
+    }
+
+    private bool IsGoingRight()
+    {
+        return Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+    }
+
+    private bool IsGoingLeft()
+    {
+        return Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+    }
+
+    private bool IsGoingUp()
+    {
+        return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+    }
+
+    private bool IsGoingDown()
+    {
+        return Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+    }
+}
